c5defcefbfdd62bf47536a1056d91a809a5603ab
freetimezp <kraftukrnet@ukr.net>
2023-05-17 17:16:20 +0300

    05. js-eatme-07-slider. Header component.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './global.css';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className='overflow-hidden bg-white w-screen h-screen'>
+      <Header />
       <Home />
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { FiLock } from 'react-icons/fi';
 
 const Header = () => {
     return (
-        <div>
-
+        <div className='container mx-auto pt-5 z-50 relative'>
+            <div className='flex justify-between'>
+                <p className='text-2xl font-bold duration-400 hover:scale-110 ease-in-out'>Food Hunt</p>
+                <div className='flex w-96 gap-10 text-xl'>
+                    {['Breakfest', 'Lunch', 'Dinner'].map((item, i) => (
+                        <p key={i} className='cursor-pointer duration-500 hover:-rotate-6 ease-in-out'>
+                            {item}
+                        </p>
+                    ))}
+                </div>
+                <FiLock className='text-3xl cursor-pointer opacity-70 hover:opacity-100 ease-in-out hover:drop-shadow-md' />
+            </div>
         </div>
     );
 };
